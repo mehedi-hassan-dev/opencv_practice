745adf670e763f1629726e0f039f08af61a1d239
Mehedihassanbabu <mehedihassanbabu8063@gmail.com>
2026-07-05 11:08:58 -0600

    Add Lesson 5 Image Transformations

---
+++ lesson5_image_transformations.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("images/image2.jpg")
+
+cv.imshow("image",img)
+
+# Translation
+def translate(img, x, y):
+    transMat = np.float32([[1, 0, x], [0, 1, y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+# -x means left
+# -y means up
+# x means right
+# y means down
+
+translated = translate(img, 100, 100)
+cv.imshow("Translated", translated)
+
+# Rotation
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow("Rotated", rotated)
+
+rotated_rotated = rotate(rotated, -45)
+cv.imshow("Rotated Rotated", rotated_rotated)
+
+# Resizing
+resized = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
+cv.imshow("Resized", resized)
+cv.waitKey(0)
